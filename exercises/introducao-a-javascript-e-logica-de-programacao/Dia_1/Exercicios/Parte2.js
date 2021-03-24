ce82f276cf95c52604c1bbfc2855594ca1d57d75
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-03-24 19:26:10 -0300

    Realizado ate a parte 6 do exercicio

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao/Dia_1/Exercicios/Parte2.js
@@ -0,0 +1,17 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let numA = 43;
+let numB = 56;
+
+if (numA > numB) {
+    console.log(numA);
+}
+else if (numA < numB) {
+    console.log(numB);
+}
+else if (numA == numB) {
+    console.log("Números iguais");
+}
+else {
+    console.log("Números inseridos inválidos");
+}
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao/Dia_1/Exercicios/Parte4.js
@@ -0,0 +1,13 @@
+// Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+numA = -4;
+
+if (numA < 0) {
+  console.log('negative');
+}
+  else if (numA == 0) {
+    console.log('zero');
+  }
+  else if (numA > 0) {
+    console.log('positive');
+  }
