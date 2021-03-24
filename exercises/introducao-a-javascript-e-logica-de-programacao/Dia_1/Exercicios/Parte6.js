d2e6acfd968a88f5de23617f446a35062fa5e5bd
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-03-24 20:07:07 -0300

    Realizado ate a parte 09 do exercicio proposto

--- exercises/introducao-a-javascript-e-logica-de-programacao/Dia_1/Exercicios/Parte6.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao/Dia_1/Exercicios/Parte6.js
@@ -5,7 +5,7 @@
 // Exemplo: bishop -> diagonals
 
 
-let chessPiece = 'KiNg';
+let chessPiece = prompt("Digite o nome da peça de xadrex em inglês");
 chessPiece = chessPiece.toLowerCase()
 
 switch(chessPiece) {
@@ -25,7 +25,7 @@ switch(chessPiece) {
     console.log('knight -> in an ‘L’ shape’: two squares in a horizontal or vertical direction, then move one square horizontally or vertically. They are the only piece able to jump over other pieces.');
       break;
   case 'pawn':
-    confirm.length('pawn -> vertically forward one square, with the option to move two squares if they have not yet moved. Pawns are the only piece to capture different to how they move. Pawns capture one square diagonally in a forward direction. Pawns are unable to move backward on captures or moves. Upon reaching the other side of the board a pawn promotes into any other piece, except for a king. Additionally, pawns can make a special move named En Passant.');
+    console.log('pawn -> vertically forward one square, with the option to move two squares if they have not yet moved. Pawns are the only piece to capture different to how they move. Pawns capture one square diagonally in a forward direction. Pawns are unable to move backward on captures or moves. Upon reaching the other side of the board a pawn promotes into any other piece, except for a king. Additionally, pawns can make a special move named En Passant.');
       break;
     default:
       console.log('Digite o nome da peça em inglês por gentileza - nome inserido inválido.')
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao/Dia_1/Exercicios/Parte8.js
@@ -0,0 +1,13 @@
+// Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false
+// Bonus: use somente um if .
+
+let numA = 5;
+let numB = 12;
+let numC = 9;
+
+if (numA % 2 == 0 || numB % 2 == 0 || numC % 2 == 0) {
+  console.log('true');
+}
+else {
+  console.log('false');
+}
