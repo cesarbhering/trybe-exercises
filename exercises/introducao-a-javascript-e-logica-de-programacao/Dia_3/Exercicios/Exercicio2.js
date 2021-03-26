8f3db0e833ddeff0a74c448220470b86c90677db
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-03-26 19:36:51 -0300

    Exercicios dia 03 bloco 04 realizados

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao/Dia_3/Exercicios/Exercicio2.js
@@ -0,0 +1,7 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+
+let numbers = 5;
+
+for (let index = 0; index < numbers; index += 1) {
+  console.log('*'.repeat(index+1));
+}
