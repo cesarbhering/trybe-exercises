a288af2e96f6f693e9577f41e3e3c9b945010742
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-26 18:29:10 -0300

    Desafio Diario do dia 01 do bloco 07 realizado - Parte 1

--- exercises/introducao-a-javascript-es6-e-testes-unitarios/Dia_1/desafio1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios/Dia_1/desafio1.js
@@ -23,3 +23,24 @@ let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
 const testingScopeArrow = escopo => escopo ? `${ifScope} ótimo, fui utilizada no escopo !` : `${elseScope}`;
 
 console.log(testingScopeArrow(true));
+
+
+
+// Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+// Copie o código abaixo.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+
+// Peguei inspiração aqui: https://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly 
+console.log(`Os números ${oddsAndEvens.sort((a,b) => a - b)} se encontram ordenados de forma crescente!`);
+
+
+
+
+
+
+
