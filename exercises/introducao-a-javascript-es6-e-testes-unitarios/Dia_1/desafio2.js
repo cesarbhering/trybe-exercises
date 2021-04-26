b91fd6748cd77b94bfa8a8620ef4c58f04a12f02
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-26 19:50:19 -0300

    Iniciando parte 2 do desafio diario bloco 07

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios/Dia_1/desafio2.js
@@ -0,0 +1,27 @@
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+
+function factorial(num) {
+  if (num <0) {
+    return 'Numero Invalido';
+  } else if (num === 0) {
+      return 1;
+  } else {
+      let buffer = num * (num - 1);
+      for (let index = 2; index < num; index += 1) {
+        buffer = buffer * (num - index);
+      }
+      return buffer;
+    }
+}
+
+console.log(factorial(7))
+
+
+// Para fazer este exercício eu consultei:
+// https://www.freecodecamp.org/news/how-to-factorialize-a-number-in-javascript-9263c89a4b38/
+
+const factorialTernary = (num) => num * factorialTernary(num - 1);
+
+console.log(factorialTernary(5));
