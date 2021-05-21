86fb7be874395e52611fb85583aff0d766a39db2
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-05-21 19:06:20 -0300

    Exercicio bonus realizado

--- exercises/Primeiros-passos-Jest/Dia_3/exercicios/Exercicio6.test.js
+++ exercises/Primeiros-passos-Jest/Dia_3/exercicios/Exercicio6.test.js
@@ -1,15 +1,17 @@
-const Exercicio6 = require('./Exercicio6');
-jest.mock = require('./Exercicio6');
+const Exercicio6 = require("./Exercicio6");
+jest.mock = require("./Exercicio6");
 
-describe('Testes solicitados no exercício 6', () => {
-  Exercicio6.dogPicture = jest.fn()
-    .mockResolvedValue('Chamada realizada com Sucesso!');
-  test('Verificar resposta mockada em resolves', () => {
-    expect(Exercicio6.dogPicture()).resolves.toBe('Chamada realizada com Sucesso!');
+describe("Testes solicitados no exercício 6", () => {
+  Exercicio6.dogPicture = jest
+    .fn()
+    .mockResolvedValue("Chamada realizada com Sucesso!");
+  test("Verificar resposta mockada em resolves", () => {
+    expect(Exercicio6.dogPicture()).resolves.toBe(
+      "Chamada realizada com Sucesso!"
+    );
   });
-  test('Verificar resposta mockada em rejects', () => {
-    Exercicio6.dogPicture = jest.fn()
-      .mockRejectedValue( new Error ('error'));
-    expect(Exercicio6.dogPicture()).rejects.toThrow(new Error ('error'))
-  } )
+  test("Verificar resposta mockada em rejects", () => {
+    Exercicio6.dogPicture = jest.fn().mockRejectedValue(new Error("error"));
+    expect(Exercicio6.dogPicture()).rejects.toThrow(new Error("error"));
+  });
 });
