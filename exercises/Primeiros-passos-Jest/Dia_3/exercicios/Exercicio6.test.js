9ca18ecb28e3cdf19ad96e7d7440377eb507749a
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-05-21 18:28:33 -0300

    Exercicio 6 realizado

---
+++ exercises/Primeiros-passos-Jest/Dia_3/exercicios/Exercicio6.test.js
@@ -0,0 +1,15 @@
+const Exercicio6 = require('./Exercicio6');
+jest.mock = require('./Exercicio6');
+
+describe('Testes solicitados no exercício 6', () => {
+  Exercicio6.dogPicture = jest.fn()
+    .mockResolvedValue('Chamada realizada com Sucesso!');
+  test('Verificar resposta mockada em resolves', () => {
+    expect(Exercicio6.dogPicture()).resolves.toBe('Chamada realizada com Sucesso!');
+  });
+  test('Verificar resposta mockada em rejects', () => {
+    Exercicio6.dogPicture = jest.fn()
+      .mockRejectedValue( new Error ('error'));
+    expect(Exercicio6.dogPicture()).rejects.toThrow(new Error ('error'))
+  } )
+});
