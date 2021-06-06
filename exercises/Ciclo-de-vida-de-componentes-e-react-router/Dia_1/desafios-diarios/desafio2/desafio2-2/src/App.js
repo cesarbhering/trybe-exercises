75112afcae6e2c3aceeeab8aa9450f86089522c3
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-06-06 17:07:58 -0300

    Exercicios de Fixacao BLoco 13 Dia 01 realizados

--- exercises/Ciclo-de-vida-de-componentes-e-react-router/Dia_1/desafios-diarios/desafio2/desafio2-2/src/App.js
+++ exercises/Ciclo-de-vida-de-componentes-e-react-router/Dia_1/desafios-diarios/desafio2/desafio2-2/src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
 import React from 'react';
-import Profile from './components/Profile';
+import Profile from './components/Profile'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.loadingPage = this.loadingPage.bind(this);
-
-    this.state = {
-      loading: false,
-    }
-  }
-
-  loadingPage() {
-    return(
-      <span> Loading ...</span>
-    )
-  }
-
   render() {
     return (
-      <div className="App">
-        {this.state.loading ? this.loadingPage : <h1>tenso</h1> }
-      </div>
+      <Profile />
     );
   }
 }
