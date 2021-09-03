1bbc3e2a4a6a20009373703cfe95a46305ce9316
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-09-03 15:07:17 -0300

    Exericicos dia 03 do Bloco de NOrmalizacao de Banco de Dados resolvidos

---
+++ exercises/NormalizacaoeMOdelagemBD/Dia_03/exercicios/exercicioVIEWS.sql
@@ -0,0 +1,55 @@
+-- Desafios sobre VIEW
+#Crie uma view chamada film_with_categories utilizando as tabelas category , film_category e film do banco de dados sakila . 
+#Essa view deve exibir o título do filme, o id da categoria e o nome da categoria, conforme a imagem abaixo. Os resultados devem ser ordenados pelo título do filme.
+USE sakila;
+CREATE VIEW film_with_categories AS
+SELECT f.title, fc.category_id, c.name FROM category as c
+INNER JOIN film_category AS fc
+ON c.category_id = fc.category_id
+INNER JOIN film AS f
+ON f.film_id = fc.film_id
+ORDER BY f.title;
+
+SELECT * FROM film_with_categories;
+
+-- DROP VIEW film_with_categories;
+
+#Crie uma view chamada film_info utilizando as tabelas actor , film_actor e film do banco de dados sakila . 
+#Sua view deve exibir o actor_id , o nome completo do ator ou da atriz em uma coluna com o ALIAS actor e o título dos filmes. 
+#Os resultados devem ser ordenados pelos nomes de atores e atrizes. Use a imagem a seguir como referência.
+CREATE VIEW film_info AS
+SELECT a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS actor, f.title FROM actor AS a
+INNER JOIN film_actor AS fc
+ON fc.actor_id = a.actor_id
+INNER JOIN film AS f
+ON  f.film_id = fc.film_id
+ORDER BY CONCAT(a.first_name, ' ', a.last_name);
+
+SELECT * FROM film_info;
+
+-- DROP VIEW film_info;
+
+#Crie uma view chamada address_info que faça uso das tabelas address e city do banco de dados sakila . 
+#Sua view deve exibir o address_id , o address , o district , o city_id e a city . 
+#Os resultados devem ser ordenados pelo nome das cidades. Use a imagem abaixo como referência.
+CREATE VIEW address_info AS
+SELECT a.address_id, a.address, a.district, a.city_id, c.city FROM address AS a
+INNER JOIN city as c
+ON c.city_id = a.city_id
+ORDER BY c.city;
+
+SELECT * FROM address_info;
+
+-- DROP VIEW address_info;
+
+#Crie uma view chamada movies_languages , usando as tabelas film e language do banco de dados sakila . 
+#Sua view deve exibir o título do filme , o id do idioma e o idioma do filme , como na imagem a seguir.
+CREATE VIEW movies_languages AS
+SELECT f.title, f.language_id, l.name FROM film AS f
+INNER JOIN `language` AS l
+ON f.language_id = l.language_id;
+
+SELECT * FROM movies_languages;
+
+-- DROP VIEW address_info;
+
