b25179751ba04dd8edd8461ba5a6d2980194d595
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-09-15 08:23:17 -0300

    Exercicios dia 02 de Updates em MongoDB realizados

--- exercises/MongoDBUpdates/Dia_2/Exercicios/Exercicios.js
+++ exercises/MongoDBUpdates/Dia_2/Exercicios/Exercicios.js
@@ -42,3 +42,10 @@ db.movies.updateOne({title: "Batman"},{$push: {cast: {$each: [{
 Adicione o campo actor , que deve ser um array com o valor Christian Bale , ao array de cast em que o campo character seja igual a Batman ;
 Adicione o campo actor , que deve ser um array com o valor Michael Caine , ao array de cast em que o campo character seja igual a Alfred ;
 Adicione o campo actor , que deve ser um array com o valor Heath Ledger , ao array de cast em que o campo character seja igual a Coringa . */
+db.movies.updateOne({title: "Batman", "cast.character": "Batman"},{$push: {"cast.$.actor": "Christian Bale"}});
+db.movies.updateOne({title: "Batman", "cast.character": "Alfred"},{$push: {"cast.$.actor": "Michael Caine"}});
+db.movies.updateOne({title: "Batman", "cast.character": "Coringa"},{$push: {"cast.$.actor": "Heath Ledger"}});
+
+/* Exercício 12: Adicione aos atores de cast do character Batman do filme Batman os valores 
+"Michael Keaton" , "Val Kilmer" e "George Clooney" , e deixe o array em ordem alfabética. */
+db.movies.updateMany({title: "Batman", "cast.character": "Batman"},{$push: {"cast.$.actor": {$each: ["Michael Keaton", "Val Kilmer", "George Clooney"], $sort: -1}}});
