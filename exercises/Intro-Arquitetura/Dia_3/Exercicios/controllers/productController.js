f48fbf0468f7eab8e6381403f674db13b8c36653
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-10-14 12:43:23 -0300

    Exercicios dia 03 de introducao a arquitetura realizados

--- exercises/Intro-Arquitetura/Dia_3/Exercicios/controllers/productController.js
+++ exercises/Intro-Arquitetura/Dia_3/Exercicios/controllers/productController.js
@@ -10,38 +10,54 @@ Exercício 3 : Utilize os verbos (POST, PUT, GET etc.) corretos para cada ação
 Exercício 4 : Garanta que todos os endpoints tenham as respostas (status code) corretas, 
 ou seja, código para quando der tudo certo, código pra quando ocorrer erro etc. */
 
-router.get('/products/list-products', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
-router.get('/products/get-by-id/:id', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  if (product === null) {
+    res.status(404).send({ message: 'Produto não encontrado' });
+  }
+
+  res.status(200).json(product);
 });
 
-router.post('/products/add-user', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
-  const newProduct = await ProductModel.add(name, brand);
+  try {
+    const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+    res.status(200).json(newProduct);
+  } catch (e) {
+    res.status(500).send({ message: 'Algo deu errado' });
+  }
 });
 
-router.delete('/users/delete-user/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+router.delete('/:id', async (req, res) => {
+  try {
+    const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(500).send({ message: 'Algo deu errado' });
+  }
 });
 
-router.put('/users/update-user/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
-  const products = await ProductModel.update(req.params.id, name, brand);
+  try {
+    const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(500).send({ message: 'Algo deu errado' });
+  }
 });
 
 module.exports = router;
